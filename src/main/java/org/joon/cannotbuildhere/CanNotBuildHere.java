f40b8e6837b3038e863056e71adcf15bf78f758d
joon0447 <joon0447@naver.com>
2024-05-02 17:40:33 +0900

    g ds

---
+++ src/main/java/org/joon/cannotbuildhere/CanNotBuildHere.java
@@ -0,0 +1,17 @@
+package org.joon.cannotbuildhere;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CanNotBuildHere extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
